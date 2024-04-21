c4faaab3e2c876f2582f47cbf223b7c8daeaf2a8
racingulitca <mex83011@gmail.com>
2024-04-21 16:19:53 +0300

    feat: WhyAreTheyTrustUs component complete

--- src/components/WhyAreTheyTrustUs/Slider/Slider.tsx
+++ src/components/WhyAreTheyTrustUs/Slider/Slider.tsx
@@ -4,6 +4,7 @@ import { SliderArr } from './Slider.typings'
 import Image from 'next/image'
 import arrow from './assets/arrow.svg'
 import commentIcon from './assets/commentIcon.svg'
+import Link from 'next/link'
 
 export default function Slider({
     sliderArr,
@@ -53,6 +54,24 @@ export default function Slider({
                                 >
                                     <div className={styles.imageContainer}>
                                         <Image src={slide.image} alt='slideImage' className={styles.slideImage} />
+                                    </div>
+                                    <div className={styles.textContainer}>
+                                        <div className={styles.slideHeader}>
+                                            <h3>{slide.title}</h3>
+                                            <Link
+                                                className={styles.articleLink}
+                                                href={slide.link}
+                                            >
+                                                Статья
+                                            </Link>
+                                        </div>
+                                        <p className={styles.text}>
+                                            Sed ut perspiciatis unde omnisiste natus error sit voluptatem accusantium doloremqu laudantium illo inventore veritatiset quas
+                                        </p>
+                                    </div>
+                                    <div className={styles.commentsBlock}>
+                                        <Image src={commentIcon} className={styles.commentIcon} alt='commentIcon' />
+                                        <span>{slide.numberOfComments}</span>
                                     </div>
                                 </div>
                             )
