5d468f8f3abf55ca6dc05bdac4237240f28db26b
racingulitka <mex83011@gmail.com>
2024-05-30 19:06:02 +0300

    feat:LandingPage Hero complete

--- src/components/LandingPage/LandingPageHero/LandingPageHero.tsx
+++ src/components/LandingPage/LandingPageHero/LandingPageHero.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import styles from './LandingPageHero.module.scss'
 import HeroBottomLinks from '@/components/common/HeroBottomLinks/HeroBottomLinks'
+import Image from 'next/image'
+import heroImage from './assets/heroImage.png'
 
-export default function LandingPageHero(){
-    return(
+export default function LandingPageHero() {
+
+    const mainTitleText = 'РАЗРАБОТКА\nLANDING PAGE'
+
+    return (
         <div className={styles.mainWrapper}>
             <div className={styles.wrapper}>
+                <div className={styles.mainFlex}>
+                    <div className={styles.actionBlock}>
+                        <div className={styles.mainInfo}>
+                            <h1 className={styles.mainTitle}>{mainTitleText}</h1>
+                            <p className={styles.slogan}>Долго, дорого и <span>эффективно</span></p>
+                        </div>
+                        <button className={styles.button}>ПОЛУЧИТЬ ПРЕДЛОЖЕНИЕ С ЦЕНАМИ</button>
+                    </div>
+                    <Image src={heroImage} alt='laptop image' className={styles.heroImage} />
+                </div>
                 <HeroBottomLinks />
             </div>
         </div>
